4e70472cf8a88a7dbd30c804cfb1e1e4041f4270
chenmfa <790109677@qq.com>
2016-05-11 15:45:57 +0800

    空格

--- src/main/java/learn/util/RequestUtil.java
+++ src/main/java/learn/util/RequestUtil.java
@@ -18,10 +18,10 @@ public class RequestUtil {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-      ip = request.getHeader("HTTP_CLIENT_IP");  
+			ip = request.getHeader("HTTP_CLIENT_IP");  
 		}  
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-      ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
 		}  
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
