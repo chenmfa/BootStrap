ec972869a2fe96b0ef930573cfb73b4bdf70e29a
chenmfa <790109677@qq.com>
2016-05-11 15:42:48 +0800

    增加ip的获取方式

--- src/main/java/learn/util/RequestUtil.java
+++ src/main/java/learn/util/RequestUtil.java
@@ -17,6 +17,12 @@ public class RequestUtil {
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+      ip = request.getHeader("HTTP_CLIENT_IP");  
+		}  
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+      ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+		}  
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
 		}
